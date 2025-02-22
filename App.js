7098f3c2754ba48e6cc6ebf4c9793f3d5009b275
Gustavo <gstvrrds@gmail.com>
2025-02-22 16:33:56 -0300

    feat:adding flexbox

--- App.js
+++ App.js
@@ -3,26 +3,32 @@ import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Another piece of text</Text>
-      <Text style={styles.text}>Hello world</Text>
-      <Button title="Tap me" />
+    <View style={styles.mainContent}>
+      <View style={styles.container}>
+        <Text>Title</Text>
+        <Text>lorem ipsum lorem ipsum</Text>
+      </View>
+      <View style={styles.container}>
+        <Text>Title</Text>
+        <Text>lorem ipsum lorem ipsum</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  mainContent: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#c6c6c6",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  container: {
     alignItems: "center",
     justifyContent: "center",
-  },
-  text: {
-    margin: 10,
-    borderWidth: 1,
-    padding: 5,
-    borderRadius: 5,
-    borderColor: "#ff00ff",
+    flexDirection: "column",
+    backgroundColor: "#ffffff",
+    padding: 10,
   },
 });
