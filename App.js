0aa8ee6229fefa72caefa5b9f246d5e6578f87b4
Gustavo <gstvrrds@gmail.com>
2025-02-25 20:18:18 -0300

    feat:adding remove goal option

--- App.js
+++ App.js
@@ -2,10 +2,17 @@ import { StatusBar } from "expo-status-bar";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 import * as Notifications from "expo-notifications";
 import { useEffect, useState } from "react";
+import Icon from "react-native-vector-icons/FontAwesome";
+
 export default function App() {
   const [value, setValue] = useState("");
   const [goals, setGoals] = useState([]);
 
+  const deleteGoal = (index) => {
+    let newGoals = [...goals];
+    newGoals.splice(index, 1);
+    setGoals(newGoals);
+  };
   const handleAdd = (e) => {
     setGoals((prevGoals) => [...prevGoals, value]); // Add the new goal to the list
     setValue(""); // Clear input after adding the goal
@@ -27,12 +34,28 @@ export default function App() {
         <Button title="Add goals" onPress={handleAdd} style={styles.button} />
       </View>
       <View>
-        <View>
+        <View style={styles.goalsContent}>
           {goals.length > 0 ? (
             goals.map((goal, index) => (
-              <Text key={index} style={{ color: "black" }}>
-                {goal}
-              </Text>
+              <View style={styles.goalContet}>
+                <Text
+                  key={index}
+                  style={{
+                    color: "black",
+                    fontSize: 20,
+                    padding: 5,
+                    borderRadius: 5,
+                  }}
+                >
+                  {goal}
+                </Text>
+                <Icon
+                  name="trash"
+                  style={{ color: "#ff0000" }}
+                  size={30}
+                  onPress={() => deleteGoal(index)}
+                />
+              </View>
             ))
           ) : (
             <Text>List of goalss...</Text>
@@ -64,6 +87,17 @@ const styles = StyleSheet.create({
     borderColor: "#000",
     borderWidth: 1,
     width: "70%",
+    padding: 10,
+  },
+  goalsContent: {
+    marginTop: 10,
+    gap: 5,
+  },
+  goalContet: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: "#c6c6c6",
+    alignItems: "center",
     padding: 10,
   },
 });
